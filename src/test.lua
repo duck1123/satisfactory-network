9598503264b2a221f7572f62434b8a00fbd3e311
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-08-16 00:00:41 -0400

    update registry

--- src/test.lua
+++ src/test.lua
@@ -82,3 +82,48 @@ end
 -- print(component.proxy(miners["2"]):getLocation())
 -- print(component.proxy(miners["3"]):getLocation())
 -- print(component.proxy("CCAB02D9441F2A2CCF42FAA698AF8C0A"):getLocation())
+
+-- modes = {"r", "w", "a", "+r", "+w", "+a"}
+
+-- f = filesystem.open(logFile, modes[2])
+-- print(f:getSize())
+-- print(f:read("*all"))
+-- print(f:getMode())
+
+
+-- f:write(computer:time())
+-- print(f:seek("end", -2))
+-- f:write(computer:time())
+-- f:close()
+
+-- print(computer:time())
+
+-- printMembers(computer)
+
+-- storageReport()
+-- pipeStorage = component.proxy(storage["motor"])
+-- debugInventories(pipeStorage)
+-- printMembers(pipeStorage)
+-- c = component.proxy("87AB9F5A4BD4EFCF1208D8A9B6085420")
+-- debugComponent(c)
+-- debugInventories(c)
+-- a = c:open()
+-- c:send("foo")
+-- print(a)
+-- filesystem.mount("/dev/" .. driveId .. "/log.txt", "/log.txt")
+-- logFile = "/log.txt"
+-- print(readData(logFile))
+-- writeData(logFile, "food")
+-- appendData(logFile, "foo")
+-- log("All work and no play makes jack a dull boy")
+-- inv = c:getInventories()[1]
+-- index, stack = inv:getStack(1)
+-- print(index)
+-- print(stack)
+-- debugTable(stack)
+-- item = stack.item
+-- debugTable(item)
+-- printMembers(inv)
+-- debugInventory(inv)
+-- debugTypes(inv)
+-- debugTypes(c)
