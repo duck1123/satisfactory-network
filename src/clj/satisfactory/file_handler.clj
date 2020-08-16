1fc93011ae37edfba53bcd8b8678498d937e7087
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-08-16 15:06:47 -0400

    send message and handlers

--- src/clj/satisfactory/file_handler.clj
+++ src/clj/satisfactory/file_handler.clj
@@ -3,10 +3,26 @@
    [clojure.java.io :as io]
    [satisfactory.config :refer [env]]))
 
+(defn send-message!
+  [message]
+  (let [inbox (io/file (env :inbox))
+        id (inst-ms (java.util.Date.))
+        outfile (io/file inbox (str id ".txt"))]
+    (spit outfile message)))
+
+(defn handle-echo
+  [_file message]
+  (send-message! message))
+
+(defn handle-ping
+  [_file _message]
+  (send-message! "pong"))
+
 (defn handle-event
   [event]
   (try
-    (let [{:keys [types]} event]
+    (let [{:keys [types]} event
+          handlers {"ping" handle-ping}]
       (when (some (partial = :create) types)
         (Thread/sleep 1000)
         (let [outbox-dir (env :outbox2)
@@ -15,13 +31,8 @@
             (when (.isFile file)
               (let [message (slurp file)]
                 (println (str (.getName file) " - " message))
-
-                (let [inbox (io/file (env :inbox))
-                      id (inst-ms (java.util.Date.))
-                      outfile (io/file inbox (str id ".txt"))]
-                  (if (= message "ping")
-                    (spit outfile "pong")
-                    (spit outfile message))))
+                (let [handler (get handlers message handle-echo)]
+                  (handler file message)))
               (.delete file))))))
     (catch Exception e
       (println e))))
