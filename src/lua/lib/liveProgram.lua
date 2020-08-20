13bf552e9b5cc2e8200aa3c21cf5c96cb11cb0dc
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-08-20 05:56:43 -0400

    reloader thread

--- src/lua/lib/liveProgram.lua
+++ src/lua/lib/liveProgram.lua
@@ -1,7 +1,35 @@
 -- taken from: https://github.com/CoderDE/FicsIt-Networks/blob/bb9f994e3211a539e89e5c9ab13afcba2d67fbbb/FicsItOS2.0/lib/liveProgram.lua
 
 function runProgramLive(path)
+   local loading = false
+   local loadingQueued = false
    local programThread = thread.create(filesystem.loadFile(path))
+
+   local reloader = thread.create(function()
+         while true do
+            if loading then
+               programThread:stop()
+               local program = filesystem.loadFile(path)
+
+               if program then
+                  status, programThread1 = pcall(thread.create, program)
+
+                  if status then
+                     programThread = programThread1
+                  end
+               end
+
+               loadingQueued = false
+            end
+
+            loading = loadingQueued
+
+            if loading then
+               loadingQueued = false
+            end
+         end
+   end)
+
    local listenerThread = thread.create(function()
          while true do
             local e,s,t,p = event.pull()
@@ -16,10 +44,10 @@ function runProgramLive(path)
 
                if e == "FileSystemUpdate" then
                   if t == 2 then
-                     programThread:stop()
-                     local program = filesystem.loadFile(path)
-                     if program then
-                        programThread = thread.create(program)
+                     if q then
+                        if not loadingQueued then
+                           loadingQueued = true
+                        end
                      end
                   end
                end
