07ac1e1f0cfd2b38fa69c0efc2ed093149f384e6
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-08-20 07:03:08 -0400

    Add logging for livepanel support

--- src/lua/lib/liveProgram.lua
+++ src/lua/lib/liveProgram.lua
@@ -4,12 +4,24 @@ function runProgramLive(path)
    local programThread = thread.create(filesystem.loadFile(path))
    local listenerThread = thread.create(function()
          while true do
-            local e,s,t,p = table.unpack(event.pull())
-            if e == "FileSystemUpdate" and t == 2 and p == path then
-               programThread:stop()
-               local program = filesystem.loadFile(path)
-               if program then
-                  programThread = thread.create(program)
+            local e,s,t,p = event.pull()
+
+            if e ~= null then
+               print(
+                  string.format(
+                     "liveprogram e = %s, s = %s, t = %s, p = %s",
+                     e, s, t, p
+                  )
+               )
+
+               if e == "FileSystemUpdate" then
+                  if t == 2 then
+                     programThread:stop()
+                     local program = filesystem.loadFile(path)
+                     if program then
+                        programThread = thread.create(program)
+                     end
+                  end
                end
             end
          end
