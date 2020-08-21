28b536f79d9b7f8fbcc3ab3d4bdb1b01df3698a0
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-08-21 08:22:44 -0400

    move event loop to main

--- src/lua/main.lua
+++ src/lua/main.lua
@@ -1,3 +1,30 @@
 print("running main7")
 
-doEventLoop()
+local registeredButtons = registerButtons()
+
+-- debugTable(buttons)
+
+while true do
+   computer.skip()
+   eventName, c, arg1, arg2, arg3 = event.pull(1)
+
+   if eventName ~= null then
+      print(
+         string.format(
+            "mainloop eventName = %s, c = %s, arg1 = %s, arg2 = %s, arg3 = %s",
+            eventName, c, arg1, arg2, arg3
+         )
+      )
+
+      if eventName == "FileSystemUpdate" then
+         if arg1 == 0 then
+            doProcessInbox()
+         end
+      end
+
+      if eventName == "Trigger" then
+         local def = findButtonDef(registeredButtons, c)
+         handleButtonTrigger(eventName, c, arg1, def)
+      end
+   end
+end
