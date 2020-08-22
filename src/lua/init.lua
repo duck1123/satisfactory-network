bdfa2131e23452667ddc9bb8b86ab60c3dc23add
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-08-22 14:42:32 -0400

    remove live program files

--- src/lua/init.lua
+++ src/lua/init.lua
@@ -1,8 +1,5 @@
 function init()
    filesystem.doFile("/lib/string.split.lua")
-   filesystem.doFile("/lib/event.lua")
-   filesystem.doFile("/lib/thread.lua")
-   filesystem.doFile("/lib/liveProgram.lua")
 
    filesystem.doFile("/config.lua")
    filesystem.doFile("/registry.lua")
