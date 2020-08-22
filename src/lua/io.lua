35a09bfcdbfc18183aa7ca2aafb5fd59e26ea39b
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-08-22 15:39:05 -0400

    convert io

--- src/lua/io.lua
+++ src/lua/io.lua
@@ -1,59 +1,61 @@
 -- io.lua
 
-function readData(filename)
-   local f = filesystem.open(filename, "r")
-   local l = f:read("*all")
-   f:close()
-   return l
-end
+io = {
+   addMessage = function(message)
+      local outboxDir = config.outboxDir
+      local path = findAvailableFile(outboxDir)
+      writeData(path, message)
+   end,
 
-function writeData(filename, data)
-   local f = filesystem.open(filename, "w")
-   f:write(data)
-   f:close()
-end
+   appendData = function(filename, data)
+      local oldData = readData(filename)
+      writeData(filename, oldData .. data)
+   end,
 
-function appendData(filename, data)
-   local oldData = readData(filename)
-   writeData(filename, oldData .. data)
-end
+   assertDirectory = function(path)
+      if (not filesystem.exists(path)) then
+         print("creating path: " .. path)
+         filesystem.createDir(path)
+      end
 
-function log(data)
-   appendData(config.logFile, computer.time() .. " - " .. data .. "\n")
-end
+      return path
+   end,
 
-function findAvailableFile(baseDir)
-   local id = math.floor(computer.time() * 10)
-   local index = 0
-   local path
-   local exists = true
+   findAvailableFile = function(baseDir)
+      local id = math.floor(computer.time() * 10)
+      local index = 0
+      local path
+      local exists = true
 
-   while exists do
-      path = string.format(
-         "%s/%s-%s.txt",
-         baseDir,
-         id,
-         index
-      )
+      while exists do
+         path = string.format(
+            "%s/%s-%s.txt",
+            baseDir,
+            id,
+            index
+         )
 
-      exists = filesystem.exists(path)
-      index = index + 1
-   end
+         exists = filesystem.exists(path)
+         index = index + 1
+      end
 
-   return path
-end
+      return path
+   end,
 
-function assertDirectory(path)
-   if (not filesystem.exists(path)) then
-      print("creating path: " .. path)
-      filesystem.createDir(path)
-   end
+   log = function(data)
+      appendData(config.logFile, computer.time() .. " - " .. data .. "\n")
+   end,
 
-   return path
-end
+   readData = function(filename)
+      local f = filesystem.open(filename, "r")
+      local l = f:read("*all")
+      f:close()
+      return l
+   end,
 
-function addMessage(message)
-   local outboxDir = config.outboxDir
-   local path = findAvailableFile(outboxDir)
-   writeData(path, message)
-end
+   writeData = function(filename, data)
+      local f = filesystem.open(filename, "w")
+      f:write(data)
+      f:close()
+   end,
+}
