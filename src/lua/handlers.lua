5edbeb097c91701380420e0e9f20f9333547678b
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-08-22 15:44:01 -0400

    convert handlers

--- src/lua/handlers.lua
+++ src/lua/handlers.lua
@@ -1,24 +1,26 @@
-function handleButton1(eventName, button, arg1, def)
-   -- print(
-   --    string.format(
-   --       "button1: %s, %s, %s, %s",
-   --       eventName,
-   --       button,
-   --       arg1,
-   --       def
-   --    )
-   -- )
-   addMessage("lights-on")
-end
+handlers = {
+   handleButton1 = function(eventName, button, arg1, def)
+      -- print(
+      --    string.format(
+      --       "button1: %s, %s, %s, %s",
+      --       eventName,
+      --       button,
+      --       arg1,
+      --       def
+      --    )
+      -- )
+      addMessage("lights-on")
+   end,
 
-function handleMessageButton(eventName, button, arg1, def)
-   addMessage(def.message)
-end
+   handleButton2 = function(eventName, button, arg1, def)
+      addMessage("ping")
+   end,
 
-function handleButton2(eventName, button, arg1, def)
-   addMessage("ping")
-end
+   handleButton3 = function(eventName, button, arg1, def)
+      addMessage("lights-out")
+   end,
 
-function handleButton3(eventName, button, arg1, def)
-   addMessage("lights-out")
-end
+   handleMessageButton = function(eventName, button, arg1, def)
+      addMessage(def.message)
+   end,
+}
