90b8fcb36a0a7ece65b2920349566e497b7f82d1
Duck Nebuchadnezzar <duck@kronkltd.net>
2020-09-09 00:42:32 -0400

    simulated request

--- src/clj/satisfactory/actions/components.clj
+++ src/clj/satisfactory/actions/components.clj
@@ -6,19 +6,35 @@
    [satisfactory.queue :as sq]
    [taoensso.timbre :as timbre]))
 
+(def simulated false)
+(def force-request true)
+
 (defn read-handler
   [request]
   (let [request-id "get-component-request"
         id (get-in request [:path-params :id])]
-    (let [item (get sq/sample-component-map id)]
-      (if item
+    (if simulated
+      (if-let [item (get sq/sample-component-map id)]
         (http/ok {:item item})
-        (http/ok {:status "queued"})))))
+        (http/ok {:status "queued"}))
+      (if-let [item (get @sq/component-info id)]
+        (do
+          (fh/send-request! "get-component" request-id {"id" id})
+          (http/ok {:item item}))
+        (do
+          (fh/send-request! "get-component" request-id {"id" id})
+          (http/ok {:status "queued"}))))))
 
 (defn index-handler
   [_request]
   (let [d (md/deferred)
         id "random-id"]
 
-    (swap! sq/component-ids (constantly sq/sample-ids))
+    (if simulated
+      (swap! sq/component-ids (constantly sq/sample-ids))
+
+      (when (or force-request (empty? @sq/component-ids))
+        (swap! sq/pending-messages assoc id d)
+        (fh/send-message! "get-components" {"id" id})))
+
     (http/ok {:items @sq/component-ids})))
